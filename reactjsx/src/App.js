ee9334d690678db6857a218ed3229e30a5429d2b
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-03-26 14:33:54 +0100

    Lifecycle methods

--- reactjsx/src/App.js
+++ reactjsx/src/App.js
@@ -3,9 +3,32 @@ import Header from "./components/header/header";
 import Main from "./components/main/main";
 
 class App extends React.Component {
+    state = {
+        time: new Date()
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            this.tick()
+        }, 1000);
+    }
+
+    componentDidUpdate() {
+        console.log("hej");
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    tick() {
+        this.setState({time: new Date()})
+    }
+
     render() {
         return(
             <>
+            <h1>The time right now is: {this.state.time.toLocaleTimeString()}</h1>
             <Header />
             <Main />
             </>
