1ea6953e0b2146f46f9ff9ed4e85d607bb2b9b58
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-02 11:17:48 +0200

    Lagt till komponenter

--- message machine/src/App.tsx
+++ message machine/src/App.tsx
@@ -1,7 +1,12 @@
+import Form from "./Components/Form";
+import Message from "./Components/Message";
+
 function App() {
   return (
     <div>
       <h1>Message Machine</h1>
+      <Form />
+      <Message />
     </div>
   );
 }
